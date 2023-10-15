d9b52dfd41b3338ffea2f397552f9ce33edb6ee5
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-10-15 17:43:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+    strings: ['Programmer', 'Web Developer', 'Learner'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+   loop: true,
+loopCount: Infinity ,
+  });
